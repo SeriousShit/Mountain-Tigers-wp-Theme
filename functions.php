6e6170226ed767a3cf279bf5f26da6ffd7db37aa
Christian Schrebe <christian@schrebe.de>
2016-02-17 15:07:08 +0100

    general layout

--- functions.php
+++ functions.php
@@ -40,16 +40,16 @@ function blankslate_widgets_init() {
     register_sidebar( array (
     'name' => __( 'Sidebar Widget Area', 'blankslate' ),
     'id' => 'primary-widget-area',
-    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-    'after_widget' => "</li>",
+    'before_widget' => '<div id="%1$s" class="w3-card-2 widget-container %2$s">',
+    'after_widget' => "</div>",
     'before_title' => '<h3 class="widget-title">',
     'after_title' => '</h3>',
     ) );
     register_sidebar( array (
     'name' => __( 'Footer Widget Area', 'blankslate' ),
     'id' => 'footer-widget-area',
-    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-    'after_widget' => "</li>",
+    'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+    'after_widget' => "</div>",
     'before_title' => '<h3 class="widget-title">',
     'after_title' => '</h3>',
     ) );
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="w3-row">
-    <section id="content" class="w3-col s12 m9 l9" role="main">
+    <section id="content" class="w3-col s12 m9 l9 w3-container" role="main">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <?php get_template_part( 'entry' ); ?>
         <?php comments_template(); ?>
