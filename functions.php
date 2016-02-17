66496a1545f13dce933f1700573e30b0d33f18e9
SeriousShit <hagen@serious-shit.de>
2016-02-17 12:43:36 +0100

    stuff

--- functions.php
+++ functions.php
@@ -41,10 +41,12 @@ function blankslate_widgets_init()
 register_sidebar( array (
 'name' => __( 'Sidebar Widget Area', 'blankslate' ),
 'id' => 'primary-widget-area',
-'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-'after_widget' => "</li>",
+'before_widget' => '
+<li id="%1$s" class="widget-container %2$s">',
+    'after_widget' => "
+</li>",
 'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
+    'after_title' => '</h3>',
 ) );
 }
 function blankslate_custom_pings( $comment )
